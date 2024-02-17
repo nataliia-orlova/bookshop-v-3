854a5fbab0633a4ee8d7ba5125da0ccd3b4af70b
Nataliia Orlova <nataliia.orlova.dev@gmail.com>
2024-02-17 16:42:08 +0100

    add shopping cart

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,10 @@
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Badge, Navbar, Nav, Container } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
 import { LinkContainer } from 'react-router-bootstrap';
+import { useSelector } from 'react-redux';
 const Header = () => {
+    //  destructure what you need from the store
+    const { cartItems } = useSelector((state) => state.cart);
     return (
         <header>
             <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
@@ -16,6 +19,18 @@ const Header = () => {
                             <LinkContainer to='/cart'>
                                 <Nav.Link>
                                     <FaShoppingCart /> Cart
+                                    {cartItems.length > 0 && (
+                                        <Badge
+                                            pill
+                                            bg='warning'
+                                            style={{ marginLeft: '5px' }}
+                                        >
+                                            {cartItems.reduce(
+                                                (a, c) => a + c.qty,
+                                                0
+                                            )}
+                                        </Badge>
+                                    )}
                                 </Nav.Link>
                             </LinkContainer>
 
